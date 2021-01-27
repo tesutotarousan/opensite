e7bc88a0959ff52d690f0de88aaecd3d6ae85e55
tesutotarousan <77438189+tesutotarousan@users.noreply.github.com>
2021-01-27 23:12:47 +0900

    Add files via upload

--- js/load.js
+++ js/load.js
@@ -1,12 +1,25 @@
+
+$box = document.getElementById('testbox');
+if($box){
+  for(i=1; i<20000; i++){
+    $img = document.createElement('img');
+    src = "img/githubpages_"+(i % 10 + 1)+".png";
+    $img.src = src;
+
+    $box.appendChild($img);
+  }  
+}
+
+
 $(window).on('load', function () {
+  $('body').removeClass('is-slide');
+  
   const spinner = document.getElementById('loading');
   setTimeout(function () {
     if (spinner) {
       spinner.classList.add('loaded');
     };
-  }, 700);
-
-  $('body').removeClass('is-slide');
+  }, 100);
 });
 
 $(function () {
